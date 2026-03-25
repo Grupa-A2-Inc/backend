5db1b2358ff61dfcfc2ff752c19498b19cf3f127
ioana <ioanacaprariu06@gmail.com>
2026-03-25 19:31:09 +0200

    add JWT generation and validation with tests

---
+++ src/main/java/org/elearning/backend/security/JwtUtil.java
@@ -0,0 +1,66 @@
+package org.elearning.backend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.elearning.backend.role.entity.RoleName;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${JWT_SECRET}")
+    private String secret;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private SecretKey getKey() {
+        return key;
+    }
+
+    public String generateAccessToken(String username, RoleName role) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", role.name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 10*60*1000)) // 10 min
+                .signWith(getKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 7*24*60*60*1000L)) // 7 zile
+                .signWith(getKey())
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUsername(String token) {
+        return validateToken(token).getSubject();
+    }
+
+    public RoleName extractRole(String token) {
+        String roleName = validateToken(token).get("role", String.class);
+        return RoleName.valueOf(roleName);
+    }
+}
