9cffe190ddd8e29a619e92f9512976bf6b7bc4de
rendiucioana <nctzen2814@gmail.com>
2026-03-25 15:02:04 +0200

    task 3 + inceput de task 5 (lipseste jwt + teste pt login)

---
+++ src/main/java/org/elearning/backend/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package org.elearning.backend.auth.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.elearning.backend.auth.dto.response.AuthResponse;
+import org.elearning.backend.auth.dto.request.LoginRequest;
+import org.elearning.backend.auth.dto.request.RegisterRequest;
+import org.elearning.backend.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
