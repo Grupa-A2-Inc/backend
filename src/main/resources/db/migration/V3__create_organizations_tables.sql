1d7917a8ee79b17d996052d626c1b4d3ed5b0571
punctIT <teodorivan2005@gmail.com>
2026-03-29 11:25:09 +0300

    schema baza de date pentru noile tabele + tabelele de la backend1

---
+++ src/main/resources/db/migration/V3__create_organizations_tables.sql
@@ -0,0 +1,15 @@
+-- =====================
+-- ORGANIZATIONS
+-- =====================
+CREATE TABLE organizations (
+                               id         UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
+                               name       VARCHAR(255) NOT NULL,
+                               owner_id   UUID         NOT NULL REFERENCES users(id),
+                               created_at TIMESTAMP    NOT NULL DEFAULT NOW(),
+                               updated_at TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
+-- =====================
+-- INDEXES
+-- =====================
+CREATE INDEX idx_organizations_owner_id ON organizations(owner_id);
