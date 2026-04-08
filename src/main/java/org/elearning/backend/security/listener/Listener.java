19cbc12da1122958b79d47f283483a77ee748f7c
AlexiaF33 <alexiafichios@yahoo.com>
2026-04-08 14:29:07 +0300

    modified the log file

--- src/main/java/org/elearning/backend/security/listener/Listener.java
+++ src/main/java/org/elearning/backend/security/listener/Listener.java
@@ -6,6 +6,10 @@ import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 
 @Component
 public class Listener{
@@ -15,9 +19,14 @@ public class Listener{
 
     @EventListener
     public void onFailure(AbstractAuthenticationFailureEvent failures) {
+
+        String formattedTimestamp = Instant.ofEpochMilli(failures.getTimestamp())
+                .atZone(ZoneId.systemDefault())
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
         logger.info("Authentication failed! user={}, timestamp={}",
                 failures.getAuthentication().getName(),
-                failures.getTimestamp()
+                formattedTimestamp
         );
     }
 }
