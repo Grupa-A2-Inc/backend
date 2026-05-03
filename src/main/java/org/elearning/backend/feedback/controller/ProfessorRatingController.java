39957865f41111e5a69ab7623b915c23f2b20bac
TeoTib <tibuleacteodor@gmail.com>
2026-05-03 03:03:05 +0300

    sprint done: added get endpoint for prof ratings + tests

---
+++ src/main/java/org/elearning/backend/feedback/controller/ProfessorRatingController.java
@@ -0,0 +1,28 @@
+package org.elearning.backend.feedback.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.elearning.backend.feedback.dto.LessonRatingFullStatsDto;
+import org.elearning.backend.feedback.service.ProfessorRatingService;
+import org.elearning.backend.security.auth.CustomUserDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/professors")
+@RequiredArgsConstructor
+public class ProfessorRatingController {
+    private final ProfessorRatingService professorRatingService;
+
+    @GetMapping("/me/lessons/ratings")
+    @PreAuthorize("hasRole('TEACHER')")
+    public ResponseEntity<List<LessonRatingFullStatsDto>> getAverageRatingsForAllLessons(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(professorRatingService.getAverageRatingsForAllLessons(userDetails.getUserId()));
+    }
+}
